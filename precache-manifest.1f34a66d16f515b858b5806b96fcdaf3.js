be0b853b3569e237241f335969e2c112270c2a5a
Yandi2020 <wangyandi0406@gmail.com>
2020-12-18 18:09:34 +0100

    Updates

---
+++ precache-manifest.1f34a66d16f515b858b5806b96fcdaf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd83379caecf98aa30f0acc45b27b4e0",
+    "url": "/React-Practice-TodoList/index.html"
+  },
+  {
+    "revision": "0186ea74336919aec988",
+    "url": "/React-Practice-TodoList/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4fe9053fb52338eb2a86",
+    "url": "/React-Practice-TodoList/static/js/2.2f255bd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Practice-TodoList/static/js/2.2f255bd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0186ea74336919aec988",
+    "url": "/React-Practice-TodoList/static/js/main.a9a4bf2f.chunk.js"
+  },
+  {
+    "revision": "4f3acf06c8195ed186a1",
+    "url": "/React-Practice-TodoList/static/js/runtime-main.ea5dcbba.js"
+  }
+]);
